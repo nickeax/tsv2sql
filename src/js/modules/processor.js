a05406041bb9b6b0be6dd35f8bc32f76e73be7af
Nick Fletcher <nickeax@gmail.com>
2021-09-12 13:09:45 +1000

    Add basic styles and begin message system

--- src/js/modules/processor.js
+++ src/js/modules/processor.js
@@ -4,11 +4,13 @@ export class Processor {
   #headersInput
   #dataInput
   #process
+  #messages
 
   constructor() {
     this.#headersInput = document.querySelector('#headersInput')
     this.#dataInput = document.querySelector('#TSVDataInput')
     this.#process = document.querySelector('#process')
+    this.#messages = document.querySelector('#messages')
     this.#process.addEventListener('click', e => this.#processData(e))
   }
 
@@ -18,11 +20,27 @@ export class Processor {
 
   #processData(e) {
     e.preventDefault()
-    if (this.#headersInput.value !== '') {
+    if (this.#headersInput.value !== '' && this.#dataInput.value !== '') {
       this.#headers = this.#headersInput.value.split('\t')
-      this.#la()
+    } else if (this.#dataInput.value != '') {
+      this.#headers = this.#dataInput.value.split('\n')[0]
+    } else {
+      this.#message('info', 'No data...')
     }
     console.log(`Let the show begin!`)
+    this.#la()
+  }
+
+  #message(type, str) {
+    this[type](str)
+  }
+
+  info(str) {
+    this.#messages.innerHTML = `<span class="info">${str}</span>`
+  }
+
+  error(str) {
+    this.#messages.innerHTML = `<span class="error">${str}</span>`
   }
 
   #la() {
